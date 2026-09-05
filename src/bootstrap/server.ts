6333c08cc22ceea25aaffdbfea7f75afa6fcbf7f
OsvaldoPedro08 <pedroosvaldo187@gmail.com>
2026-09-05 08:46:24 -0300

    remoção da API_PORT no arquivo .env

--- src/bootstrap/server.ts
+++ src/bootstrap/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
 
-const API_PORT = process.env.API_PORT ?? 3030
+const API_PORT = process.env.PORT ?? 3030
 
 app.listen(API_PORT, () => {
     console.log("Servidor rodando na porta localhost:" + API_PORT)
