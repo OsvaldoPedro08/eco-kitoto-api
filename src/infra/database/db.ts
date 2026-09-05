97709aae7de590599ef315e10ea0f059a4b05998
OsvaldoPedro08 <pedroosvaldo187@gmail.com>
2026-09-05 11:02:50 -0300

    adicao do certificdo sll no cliente pool do drizzle

--- src/infra/database/db.ts
+++ src/infra/database/db.ts
@@ -3,7 +3,10 @@ import { drizzle } from "drizzle-orm/node-postgres"
 import { Pool } from "pg"
 
 const pool = new Pool({
-    connectionString : process.env.DATABASE_URL!
+    connectionString : process.env.DATABASE_URL!,
+    ssl : {
+        rejectUnauthorized : false, // permite certificados do supabase/Pooler
+    }
 })
 
 export const db = drizzle({ client : pool })
